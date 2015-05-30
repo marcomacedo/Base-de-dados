67130484edddcf72731b018028b4e367df44a0b5
EspinhaGit <ruiespinharibeiro@ua.pt>
2015-05-30 17:55:52 +0100

    UDF

---
+++ SQL - Database/UDF.sql
@@ -0,0 +1,19 @@
+GO
+Create Function historico_encomendas() returns TABLE
+
+as
+	return (SELECT * FROM Encomenda);
+
+GO
+Create Function getDadosEncomendasByID() returns TABLE
+	
+as
+	return (SELECT DE.Data_expedicao, Ed.Nome, TE.TipoEncomenda, D.titulo FROM Encomenda As E 
+				JOIN Discos_encomenda As DE On E.id_encomenda = DE.id_encomenda 
+				JOIN Editora As Ed On DE.id_Editora = Ed.id_editora
+				JOIN TipoEncomenda As TE On E.id_tipoEncomenda = TE.id_tipoEncomenda
+				JOIN Discos As D On D.id_disco = DE.id_disco
+				JOIN Cliente As C On C.NIF = E.NIF_Cliente
+				Join Pagamento As Pg On E.id_pagamento = Pg.id_pagamento)
+GO
+SELECT * FROM getDadosEncomendasByID()
